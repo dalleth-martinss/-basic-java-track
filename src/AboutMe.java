8b0dec0e540415c5ae15742494cee79141f5ff3a
Dalleth <essi_dalleth@outlook.com>
2024-02-24 15:12:09 -0300

    Adding try and catch

---
+++ src/AboutMe.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+   public static void main(String[] args) {
+
+      try {
+
+         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+         System.out.println("Digite seu nome ");
+         String nome = scanner.next();
+
+         System.out.println("Digite seu sobrenome ");
+         String sobreNome = scanner.next();
+
+         System.out.println("Digite sua idade ");
+         int idade = scanner.nextInt();
+
+         System.out.println("Digite sua altura ");
+         double altura = scanner.nextDouble();
+
+         System.out.println("Olá, me chamo " + nome.toUpperCase() + " ");
+         System.out.println("Tenho " + idade + " anos");
+         System.out.println("Minha altura é: " + altura + "cm");
+      } catch (InputMismatchException e) {
+         System.out.println("Os campos idade e altura precisam ser escritos em alfanumericos");
+      }
+
+   }
+
+}
